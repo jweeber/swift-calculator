23641171a8759c49d9488011378e2927b58d7dfb
Jessica <jessica.weeber@gmail.com>
2017-10-15 18:47:21 -0700

    Display pressed digits to the user in the calculator display

--- swift-calculator/ViewController.swift
+++ swift-calculator/ViewController.swift
@@ -3,12 +3,25 @@
 //  swift-calculator
 //
 //  Created by Jessica on 10/15/17.
-//  Copyright © 2017 jess.weeber.calculator. All rights reserved.
+//  Copyright © 2017 Jessica Weeber. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    var isTyping = false
 
+    @IBOutlet weak var display: UILabel!
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if isTyping {
+            let digitsInDisplay = display.text!
+            display.text! = digitsInDisplay + digit
+        } else {
+            display.text! = digit
+            isTyping = true
+        }
+    }
 }
 
